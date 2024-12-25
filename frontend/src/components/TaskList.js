aad23b16d49e42e3f31e52880e9b39753a4314c3
charitharedd <129164869+charitharedd@users.noreply.github.com>
2024-12-25 09:35:14 +0530

    Update TaskList.js

--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -1,1 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
+const TaskList = ({ tasks, onEdit, onDelete }) => {
+    return (
+        <div>
+            {tasks.map(task => (
+                <div key={task._id}>
+                    <h3>{task.name}</h3>
+                    <p>{task.description}</p>
+                    <p>{task.dueDate}</p>
+                    <button onClick={() => onEdit(task)}>Edit</button>
+                    <button onClick={() => onDelete(task._id)}>Delete</button>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default TaskList;

ee3874754410ea24591a500c0e975b84d624c5c7
charitharedd <129164869+charitharedd@users.noreply.github.com>
2024-12-25 09:27:47 +0530

    Update Task.js

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
 
+const taskSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    dueDate: { type: Date, required: true },
+    status: { type: String, enum: ['Pending', 'In Progress', 'Completed'], default: 'Pending' },
+    priority: { type: String, enum: ['Low', 'Medium', 'High'], required: true },
+});
+
+module.exports = mongoose.model('Task', taskSchema);
